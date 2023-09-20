998c9d91704103392215429e04b4d4962cafd40b
Alysson Carvalho <alyssonper@gmail.com>
2023-09-20 17:41:59 -0300

    .

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,9 @@
-import React from 'react'
 
 const Footer = () => {
   return (
     <footer className='footer-container'>
       <div className='footer'>
-        <h1>Let's work together!</h1>
+        <h1>Contact</h1>
           <ul>
               <li>📧 alyssonper@gmail.com</li>
               <li>📞 +55 (87) 99999-99999</li>
